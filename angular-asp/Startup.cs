7810815e9d537001b043fb112f2fe1698fdc4238
Kelvin Yan <keyan@Kelvins-MacBook-Pro.local>
2017-07-02 10:50:31 -0700

    Complete

--- angular-asp/Startup.cs
+++ angular-asp/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.IO;
+
 
 namespace angular_asp
 {
